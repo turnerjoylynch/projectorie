f6a24cdd0b8bf8efa7bbc506c49934c678af2ac8
turner lynch <turnerjoylynch@gmail.com>
2022-04-08 20:00:25 -0700

    routing fix

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export const Home = () => (
     <>
-        <section class="home__section" >
+        <section className="home__section" >
         <h2>Projectorie</h2>
         <small>For planning projects with simplicity.</small>
-        <img class="home__img"/>
+        <img className="home__img"/>
         </section>
     </>
 )
